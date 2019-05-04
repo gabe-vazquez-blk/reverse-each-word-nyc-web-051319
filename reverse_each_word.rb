e780ca787a56c35c01138b6f453ecc9299902b8f
Gabe Vazquez <gabe.vazquez@blackrock.com>
2019-05-04 21:05:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,13 @@
+#def reverse_each_word(sentence)
+  #new_sentence = []
+  #sentence = sentence.split(" ")
+  #sentence.each do |w|
+    #w = w.reverse!
+    #new_sentence << w
+  #end
+  #new_sentence.join(" ")
+#end
+
 def reverse_each_word(sentence)
   new_sentence = []
   sentence = sentence.split(" ")
